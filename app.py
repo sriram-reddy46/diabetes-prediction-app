e4225b210a9c55b8e717ed3e77e11fc6ac088f8b
sriram reddy <153343334+sriram-reddy46@users.noreply.github.com>
2026-02-28 11:00:42 +0530

    Enhance input fields with value ranges and defaults
    Updated input fields to include min and max values with default settings.

--- app.py
+++ app.py
@@ -4,31 +4,18 @@ import pickle
 
 model, scaler = pickle.load(open("diabetes_model.pkl", "rb"))
 
+
 st.title("Diabetes Prediction App")
 
+preg = st.number_input("Pregnancies (0 – 17)", min_value=0, max_value=17, value=0)
+glucose = st.number_input("Glucose (70 – 180)", min_value=70, max_value=180, value=0)
+bp = st.number_input("Blood Pressure (60 – 120)", min_value=60, max_value=120, value=0)
+skin = st.number_input("Skin Thickness (10 – 50)", min_value=10, max_value=50, value=0)
+insulin = st.number_input("Insulin (15 – 276)", min_value=15, max_value=276, value=0)
+bmi = st.number_input("BMI (18.5 – 40)", min_value=18.5, max_value=40.0, value=0.0)
+dpf = st.number_input("Diabetes Pedigree Function (0.1 – 2.5)", min_value=0.1, max_value=2.5, value=0.0)
+age = st.number_input("Age (21 – 60)", min_value=21, max_value=60, value=21)
 
-st.subheader("Reference Ranges")
-
-st.markdown("""
-- **Pregnancies:** 0 – 17  
-- **Glucose:** 70 – 180  
-- **Blood Pressure:** 60 – 120  
-- **Skin Thickness:** 10 – 50  
-- **Insulin:** 15 – 276  
-- **BMI:** 18.5 – 40  
-- **DPF:** 0.1 – 2.5  
-- **Age:** 21 – 60  
-""")
-
-
-preg = st.number_input("Pregnancies")
-glucose = st.number_input("Glucose")
-bp = st.number_input("Blood Pressure")
-skin = st.number_input("Skin Thickness")
-insulin = st.number_input("Insulin")
-bmi = st.number_input("BMI")
-dpf = st.number_input("Diabetes Pedigree Function")
-age = st.number_input("Age")
 
 if st.button("Predict"):
     input_data = np.array([[preg, glucose, bp, skin, insulin, bmi, dpf, age]])
@@ -39,4 +26,5 @@ if st.button("Predict"):
         st.error("Diabetic")
     else:
         st.success("Non-Diabetic")
+
 
